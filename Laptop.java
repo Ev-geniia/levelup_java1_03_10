54194c29a6adaa0771922e7b2138205f36c8282b
Ev-geniia <32538278+Ev-geniia@users.noreply.github.com>
2017-10-17 11:45:18 +0300

    Hometask lesson 3-4

---
+++ Laptop.java
@@ -0,0 +1,38 @@
+public class Laptop {
+    private double price;
+    private double weight;
+
+    //constructor with params
+    public Laptop(double price, double weight) {
+        this.price = price;
+        this.weight = weight;
+    }
+    //constructor without params
+    Laptop() {
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public double getWeight() {
+        return weight;
+    }
+
+    public void setWeight(double weight) {
+        this.weight = weight;
+    }
+
+    @Override
+    public String toString() {
+        return "Laptop{" +
+                "price=" + price +
+                ", weight=" + weight +
+                '}';
+    }
+    
+}
